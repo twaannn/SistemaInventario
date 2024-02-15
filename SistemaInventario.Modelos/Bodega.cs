f85fb75f79744acbf0a499dcfa5cd2d1777599e4
twaannn <73antocol@gmail.com>
2024-02-14 20:43:32 -0500

    Configuracion del Proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [MaxLength(60, ErrorMessage ="El nombre debe ser máximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerido")]
+        [MaxLength(100, ErrorMessage = "La descripción debe ser máximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
